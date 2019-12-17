6989b3074e7190de81692f9b96901adf67dda99a
Yoshi <yoshis1057@gmail.com>
2019-12-18 01:30:53 +0900

    fix migration file

--- database/migrations/2019_12_16_055806_create_slack_users_table.php
+++ database/migrations/2019_12_16_055806_create_slack_users_table.php
@@ -13,21 +13,21 @@ class CreateSlackUsersTable extends Migration
      */
     public function up()
     {
+        Schema::create('slack_teams', function (Blueprint $table) {
+            $table->bigIncrements('id');//tableの主キー
+            $table->string('team_id')->unique();//slackのチームのid
+            $table->string('team_name');//チームの名前
+        });
+
         Schema::create('slack_users', function (Blueprint $table) {
             $table->bigIncrements('id');//tableの主キー
-            $table->string('slack_id');//slackのユーザーのid
+            $table->string('slack_id')->unique();;//slackのユーザーのid
             $table->string('team_id');//チームのid
             $table->string('name');//slackからとれる名前
             $table->boolean('is_owner');//管理者かどうかの判別
             $table->string('mode');//バイトで、8時間しか働けない者と、8時間以上働ける者を区別するため
 
             $table->foreign('team_id')->references('team_id')->on('slack_teams')->onDelete('cascade');;
-        });
-
-        Schema::create('slack_teams', function (Blueprint $table) {
-            $table->bigIncrements('id');//tableの主キー
-            $table->string('team_id');//slackのチームのid
-            $table->string('team_name');//チームの名前
         });
     }
 
