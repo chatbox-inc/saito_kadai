bf6b23d5d7f847ca40e1799fb21d2f0c71b2ae66
Yoshi <yoshis1057@gmail.com>
2020-01-11 00:46:25 +0900

    ListコマンドとJobUpdateコマンドのクラス作成

---
+++ app/Http/Controllers/Shukkin/JobUpdateController.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Http\Controllers\Shukkin;
+
+
+use App\Http\Controllers\Controller;
+
+class JobUpdateController extends Controller
+{
+
+}
---
+++ app/Http/Controllers/Shukkin/ListController.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Http\Controllers\Shukkin;
+
+
+use App\Http\Controllers\Controller;
+use App\Services\SlackService;
+
+class ListController extends Controller
+{
+
+}
