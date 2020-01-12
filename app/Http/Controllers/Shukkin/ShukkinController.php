d267db8d734b6c0aa1010d86f26dda63c3be4b00
Yoshi <yoshis1057@gmail.com>
2020-01-13 00:26:36 +0900

    slackAppが返す文字を改行するように変更

--- app/Http/Controllers/Shukkin/ShukkinController.php
+++ app/Http/Controllers/Shukkin/ShukkinController.php
@@ -22,21 +22,22 @@ class ShukkinController extends Controller
         $response = $this->response->workMessage($payload);
 
         if($response['status'] == -1) {
-            $message = '正しい入力値ではありません！ex)のように入力してください！！！ex)/shukkin 0101 13:00 19:00';
+            $message = '正しい入力値ではありません！'."\n".'以下のように入力してください！！！'."\n".'ex)/shukkin 0101 13:00 19:00';
         }
         else if($response['status'] == 0) {
             $message = '過去に登録はできません！';
         }
         else if($response['status'] == 1) {
-            $message = '人数が５人に達しています！シフト登録できません！代表に連絡してください';
+            $message = '人数が５人に達しています！'."\n".'シフト登録できません！代表に連絡してください';
         }
         else if($response['status'] == 2) {
-            $message = '申し込まれた時間では働けません!!'.'今週の残り勤務可能時間は'.$response['weekHour'].'時間'.$response['weekMin'].'分です。'
-                .'今月の残り勤務可能時間は'.$response['monthHour'].'時間'.$response['monthMin'].'分です。';
+            $message = '申し込まれた時間では働けません!!'."\n".
+                '今週の残り勤務可能時間は'.$response['weekHour'].'時間'.$response['weekMin'].'分です。'."\n".
+                '今月の残り勤務可能時間は'.$response['monthHour'].'時間'.$response['monthMin'].'分です。';
         }
         else if($response['status'] == 3) {
-            $message = '今週の残り勤務可能時間は'.$response['weekHour'].'時間'.$response['weekMin'].'分です。'
-                .'今月の残り勤務時間は'.$response['monthHour'].'時間'.$response['monthMin'].'分です。';
+            $message = '今週の残り勤務可能時間は'.$response['weekHour'].'時間'.$response['weekMin'].'分です。'."\n".
+                '今月の残り勤務可能時間は'.$response['monthHour'].'時間'.$response['monthMin'].'分です。';
         }
         else if($response['status'] == 4) {
             $message = '1日に働ける時間は7時間です！';
