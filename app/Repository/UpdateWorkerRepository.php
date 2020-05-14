57db53abc0b8d4245a3da90e9440a188e4dd95ed
Yoshi <yoshis1057@gmail.com>
2020-05-14 23:29:14 +0900

    update

--- app/Repository/UpdateWorkerRepository.php
+++ app/Repository/UpdateWorkerRepository.php
@@ -12,17 +12,15 @@ class UpdateWorkerRepository implements UpdateWorkerRepositoryInterface
     public function update($payload)
     {
         $owner = SlackUser::where('slack_id', $payload['user_id'])->first()->is_owner;
-        if(!$owner) {
-            return 'このコマンドは管理者のみしか使えません!';
-        }
+        if(!$owner) return 'このコマンドは管理者のみしか使えません!';
 
-        try {
-            $user = SlackUser::where('name', $payload['text'])->first();
-            $user->mode = '熟練者';
-            $user->save();
-            return $payload['text'].'のシフト時間制限がなくなりました';
-        }catch(Exception $e) {
-            return 'そのような人物は存在しません!';
-        }
+
+        $user = SlackUser::where('name', $payload['text'])->first();
+        if(!($user instanceof SlackUser)) return 'そのような人物は存在しません!';
+
+
+        $user->mode = '熟練者';
+        $user->save();
+        return $payload['text'].'のシフト時間制限がなくなりました';
     }
 }
